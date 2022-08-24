120cbe1ffda964ad26aaf5fbd60d3ce2174e538f
earlhalasan <earl.halasan@gmail.com>
2022-08-24 16:53:36 -0400

    setting up

--- controllers/weather.js
+++ controllers/weather.js
@@ -2,20 +2,26 @@
 // import dependencies
 /////////////////////////////////
 // this allows us to load our env variables
-const express = require('express')
-
+const express = require("express");
+const fetch = require("node-fetch");
 
 ////////////////////////////////////////////
 // Create router
 ////////////////////////////////////////////
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('show')
-})
+router.get("/", (req, res) => {
+  res.render("show");
+});
 
+router.post("/", (req, res) => {
+  const zip = req.body.zip;
+});
+
+const requestURL =
+  "api.openweathermap.org/data/2.5/weather?zip={ZIP CODE},us&units=imperial&appid={API KEY}";
 
 ////////////////////////////////////////////
 // Export the Router
 ////////////////////////////////////////////
-module.exports = router
+module.exports = router;
